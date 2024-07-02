4b22ba19d86a8326d6e63b60f8d3d9d230ad4c88
calvinyeungck <calvinyeungck@gmail.com>
2024-07-02 15:01:56 +0900

    update

--- preprocessing/example/statsbomb_skillcorner_data.py
+++ preprocessing/example/statsbomb_skillcorner_data.py
@@ -4,7 +4,11 @@ from statsbombpy import sb
 from tqdm import tqdm
 import json
 import pdb
-import sports.event_data.load_data.load_statsbomb_skillcorner as load_statsbomb_skillcorner
+import sys
+import os
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(parent_dir)
+import sports.event_data.load_data as load_data
 
 #Statsbomb API
 creds = {"user": "input your Statsbomb api user name here", "passwd": "input your Statsbomb api password here"}
@@ -24,7 +28,7 @@ def convert_df_in_dict(d):
         elif isinstance(value, dict):
             convert_df_in_dict(value)
     return d
-
+# Get Statsbomb matches data
 matches = sb.matches(competition_id=11, season_id=281, creds=creds)
 matches["competition_id"] = 11
 matches["season_id"] = 281
@@ -36,7 +40,7 @@ matches = matches[cols]
 matches.to_csv(os.path.join(save_dir, "matches.csv"), index=False)
 
 
-# Get lineups and events
+# Get Statsbomb lineups and events
 os.makedirs(os.path.join(save_dir, "lineups"), exist_ok=True)
 os.makedirs(os.path.join(save_dir, "events"), exist_ok=True)
 for match_id in tqdm(matches["match_id"].unique()):
@@ -52,12 +56,12 @@ for match_id in tqdm(matches["match_id"].unique()):
 #load_statsbomb_skillcorner
 statsbomb_match_id=3894907 #for the match id matching refer to id_matching.csv
 skillcorner_match_id=1553748
-output_dir=os.getcwd()+"/test/sports/event_data/data/statsbomb_skillcorner"
+output_dir="data.csv"
 
 statsbomb_skillcorner_event_path=save_dir+'/events'
 statsbomb_skillcorner_tracking_path=tracking_path
 statsbomb_skillcorner_match_path=match_id_path
-statsbomb_skillcorner_df=load_statsbomb_skillcorner(statsbomb_skillcorner_event_path,statsbomb_skillcorner_tracking_path,
+statsbomb_skillcorner_df=load_data.load_statsbomb_skillcorner(statsbomb_skillcorner_event_path,statsbomb_skillcorner_tracking_path,
                                                     statsbomb_skillcorner_match_path,statsbomb_match_id,skillcorner_match_id)
 statsbomb_skillcorner_df.to_csv(output_dir,index=False)
 
