c5c45ef8936577b0ee5eb538f787f46eda822ca5
Calvin Yeung <70192523+calvinyeungck@users.noreply.github.com>
2025-03-25 12:38:53 +0900

    Update space_class.py

--- preprocessing/sports/space_data/space_class.py
+++ preprocessing/sports/space_data/space_class.py
@@ -2,7 +2,7 @@ from .basketball.basketball_space_class import basketball_space_data
 
 class Space_data:
     # Modified the sports list to only include fully supported providers
-    sports = ['statsbomb', 'datastadium']
+    sports = ['provider_1', 'provider_2']
 
     def __new__(cls, data_provider, *args, **kwargs):
         if data_provider in cls.sports:
@@ -13,8 +13,7 @@ class Space_data:
             raise ValueError(f'Unknown data provider: {data_provider}')
 
 if __name__ == '__main__':
-    # Test block remains unchanged, using a supported provider ('datastadium')
-    datastadium_path = "./JLeagueData/Data_20200508/"
-    match_id = "2019091416"
-    config_path = "data/dss/config/preprocessing_dssports2020.json"
-    Space_data(data_provider='datastadium', data_path=datastadium_path, match_id=match_id, config_path=config_path).load_data()
+    # Test block remains unchanged, using a supported provider 
+    provider_path = "./provider_1/Data/"
+    match_id = "1234"
+    Space_data(data_provider='provider_1', data_path = provider_path,...).load_data()
