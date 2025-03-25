50e13199d0798aaa16497e463c95bf83b9a5d392
Calvin Yeung <70192523+calvinyeungck@users.noreply.github.com>
2025-03-25 12:39:16 +0900

    Update basketball_space_class.py

--- preprocessing/sports/space_data/basketball/basketball_space_class.py
+++ preprocessing/sports/space_data/basketball/basketball_space_class.py
@@ -3,8 +3,9 @@ import pandas as pd
 from tqdm import tqdm
 
 class Basketball_space_data:
-  def __init__(self,data_provider,space_data_path,...)
+  def __init__(self,data_provider,data_path,...)
     self.data_provider = data_provider
+    self.data_path = data_path
     ...
     
   def load_data(self):
