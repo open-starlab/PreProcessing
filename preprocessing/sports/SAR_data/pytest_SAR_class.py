799d1afb85c69823434e3f4a35c5c355f85ab781
Kenjiro ide <ide.kenjiro@g.sp.m.is.nagoya-u.ac.jp>
2025-04-21 15:27:26 +0900

    fix pytest code

--- preprocessing/sports/SAR_data/pytest_SAR_class.py
+++ preprocessing/sports/SAR_data/pytest_SAR_class.py
@@ -1,27 +1,30 @@
 from .soccer.soccer_SAR_class import Soccer_SAR_data
+from .SAR_class import SAR_data
 
-class SAR_data:
-    sports = ['statsbomb_skillcorner', 'datastadium']
 
-    def __new__(cls, data_provider, *args, **kwargs):
-        if data_provider in cls.sports:
-            return Soccer_SAR_data(data_provider, *args, **kwargs)
-        elif data_provider == "statsbomb":
-            raise NotImplementedError('StatsBomb data not implemented yet')
-        else:
-            raise ValueError(f'Unknown data provider: {data_provider}')
-  
+datastadium_path = "data/dss/raw/"
+match_id_dss = "0001"
+config_path_dss = "data/dss/config/preprocessing_dssports2020.json"
 
-if __name__ == '__main__':
-    #check if the Soccer_event_data class is correctly implemented
+statsbomb_skillcorner_path = "data/stb_skc/raw"
+match_id_laliga = "1317846"
+config_path_skc = "data/stb_skc/config/preprocessing_statsbomb_skillcorner2024.json"
+statsbomb_skillcorner_match_id = "preprocessing/sports/SAR_data/match_id_dict.json"
 
-    datastadium_path = "data/dss/raw/"
-    match_id = "0001"
-    config_path = "data/dss/config/preprocessing_dssports2020.json"
-    SAR_data(data_provider='datastadium', data_path=datastadium_path, match_id=match_id, config_path=config_path).load_data()
 
-    statsbomb_skillcorner_path = "data/stb_skc/raw"
-    match_id = "1317846"
-    config_path = "data/stb_skc/config/preprocessing_statsbomb_skillcorner2024.json"
-    statsbomb_skillcorner_match_id = "preprocessing/sports/SAR_data/match_id_dict.json"
-    SAR_data(data_provider='statsbomb_skillcorner', data_path=statsbomb_skillcorner_path, statsbomb_skillcorner_match_id=statsbomb_skillcorner_match_id, match_id=match_id, config_path=config_path).load_data()
+def test_datastadium_load_data():
+    sar = SAR_data(data_provider='datastadium', data_path=datastadium_path, match_id=match_id_dss, config_path=config_path_dss)
+    sar.load_data()
+    assert sar is not None
+
+
+def test_statsbomb_skillcorner_load_data():
+    sar = SAR_data(
+        data_provider='statsbomb_skillcorner',
+        data_path=statsbomb_skillcorner_path,
+        statsbomb_skillcorner_match_id=statsbomb_skillcorner_match_id,
+        match_id=match_id_laliga,
+        config_path=config_path_skc
+    )
+    sar.load_data()
+    assert sar is not None
