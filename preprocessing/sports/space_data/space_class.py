3944751a3758a46cf9434f170557ff3be6f6b910
Calvin Yeung <70192523+calvinyeungck@users.noreply.github.com>
2025-04-22 18:58:33 +0900

    Update space_class.py

--- preprocessing/sports/space_data/space_class.py
+++ preprocessing/sports/space_data/space_class.py
@@ -1,4 +1,3 @@
-from .basketball.basketball_space_class import Basketball_space_data
 
 class Space_data:
     # Modified the sports list to only include fully supported providers
@@ -6,6 +5,7 @@ class Space_data:
 
     def __new__(cls, data_provider, *args, **kwargs):
         if data_provider in cls.sports:
+            from .basketball.basketball_space_class import Basketball_space_data
             # If the data_provider is in the supported list, return an instance of Soccer_SAR_data
             return Basketball_space_data(data_provider, *args, **kwargs)
         else:
